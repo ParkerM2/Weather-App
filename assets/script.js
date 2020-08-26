3965296c48c8a11351b53d9e4e2bff8bcda9eb58
Parker Manning <parkerlmanning@hotmail.com>
2020-08-26 15:47:40 -0500

    ready to start on for loop to add in new divs

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,6 @@
-// setting variables from html
+
 $(document).ready(function () {
-	var iconElement = document.querySelector(".card-img-top")
-	var tempElement = document.querySelector(".temperature")
-	var descElement = document.querySelector(".temperature-description p")
-	var locationElement = document.querySelector(".city-state p")
-	var dateOf = document.querySelector(".dateOf p")
-	var kelvin = 273;
-	
+
 	// function celsiusToFahrenheit(temperature) {
 	// 	return (temperature * 9 / 5) + 32;
 	// }
@@ -24,26 +18,28 @@ $(document).ready(function () {
 			url: queryURL,
 			method: "GET"
 		}).then(function (response) {
-			var temp = response.list[0].main.temp;
-			temp = Math.floor(temp * 9 / 5 - 459);
-		/// *** ASK BCS HOW TO SET THIS UP TO ADD THE NEXT CARDS for the FUTURE DATES IDK  HOW
-		/// possibly check our activities folder from 3rd party apis
-			var temperature = $(".temperature").text(temp);
-			var tempDescription = $(".temp-description").text(response.list[0].weather[0].description)
-			// var tempImage = $("<img>").attr("src", response.list[0].weather[0].icon);
-			var cityOf = $(".city").text(response.city.name);
-			var date = $(".dateOf").text(response.list[0].dt_txt);
-			console.log(date)
-			
-			$(".weatherCard").append(temperature, cityOf, date);
-			
-		
-			// $(".temperature").text(temp);
-			// $(".temp-description").text(response.list[0].weather[0].description);
-			// $(".city").text(response.city.name);
-			// $(".dateOf").text(response.list[0].dt_txt);
-		
+			// calls weather function for displaying/grabbing info from api
+			displayWeather(response);
+	
 		})		
+			function displayWeather(response) {
+				//for loop to go through the list and tell it to print out the stats of that corresponding day into a new div
+			for (i = 0; i > response.list.length; i + 5)
+			console.log(response.list[i])
+			var temp = response.list[i].main.temp;
+			temp = Math.floor(temp * 9 / 5 - 459);
+			/// *** ASK BCS HOW TO SET THIS UP TO ADD THE NEXT CARDS for the FUTURE DATES IDK  HOW
+			/// possibly check our activities folder from 3rd party apis
+			var temperature = $(".temperature").text(temp + " ° F");
+			var tempDescription = $(".temp-description").text("Daily Forecast : " + response.list[i].weather[0].description)
+			// var tempImage = $("<img>").attr("src", response.list[0].weather[0].icon);
+			var cityOf = $(".city").text("Weather Details For : " + response.city.name);
+			var windSpeed = $(".wind-speed").text("Wind Speed : " + response.list[i].wind.speed);
+			var humidity = $(".humidity").text("Humidity : " + response.list[i].main.humidity +" %");
+			var date = $(".dateOf").text("Current Date : " + response.list[i].dt_txt);
+			$(".weather").append($(".#weatherCard"))
+			$("#weatherCard").text(temperature, tempDescription, cityOf, date, windSpeed, humidity);	
+		}
 	})
 })
 //response.list[0].main.temp * 9 / 5 - 459 ** for loop i = list.length ** run through i + (however many 3-4 to next day)
