dd21493353e3a99958b8e94ec6864991a16fa7da
Parker Manning <parkerlmanning@hotmail.com>
2020-08-26 00:28:00 -0500

    fixed console log response, working on button

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var tempElement = document.querySelector(".temperature p")
 var descElement = document.querySelector(".temperature-description p")
 var locationElement = document.querySelector(".city-state p")
 var dateOf = document.querySelector(".dateOf p")
-var kelvin = 273;
+
 
 // Creating Weather Object
 // const weather = {
@@ -19,18 +19,21 @@ var kelvin = 273;
 // };
 
 
-document.querySelector("#city")
-document.querySelector("#state")
-// Pulling from API ***https://rapidapi.com/community/api/open-weather-map?endpoint=apiendpoint_f719676c-072b-4a2d-ad2e-78f8375ea9c8**
-var city = "austin"
-var state = "tx"
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "," + state + "&appid=aadef2e30a8efe5bb715019df5f2a42a"
+// This button will call the searched city through an AJAX Call to openweathermap.org
+$("#button").on("click", function (event) {
+	event.preventDefault();
+	var city = document.querySelector("#city").val();
+	var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=aadef2e30a8efe5bb715019df5f2a42a"
 
-$.ajax({
-	url: queryURL,
-	method: "GET"
-}).then(function (response) {
-	console.log(response)
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).then(function (response) {
+		console.log(response)
+		$(".weatherCard").text(JSON.stringify(response))
+	})
 })
+
+
 
 
