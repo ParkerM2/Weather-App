c204710c94288270722ca04992191608d52f7279
Parker Manning <parkerlmanning@hotmail.com>
2020-08-26 19:08:04 -0500

    finished for loop, working on moment.js coversion

--- assets/script.js
+++ assets/script.js
@@ -20,79 +20,34 @@ $(document).ready(function () {
 		}).then(function (response) {
 			// calls weather function for displaying/grabbing info from api
 			displayCurrentWeather(response);
-			displayFutureWeatherOne(response);
-			displayFutureWeatherTwo(response);
-			displayFutureWeatherThree(response);
-			displayFutureWeatherFour(response);
+		
 	
 		})		
 			function displayCurrentWeather(response) {
 			//for loop to go through the list and tell it to print out the stats of that corresponding day into a new div
-			console.log(response)
-			console.log(response.list[0])
-			var temp = response.list[0].main.temp;
-			temp = Math.floor(temp * 9 / 5 - 459);
-			var temperature = $(".temperature1").text(temp + " ° F");
-			console.log(temp)
-			var tempDescription = $(".temp-description1").text("Daily Forecast : " + response.list[0].weather[0].description)
-			// var tempImage = $("<img>").attr("src", response.list[0].weather[0].icon);
-			var cityOf = $(".city1").text("Weather Details For : " + response.city.name);
-			var windSpeed = $(".wind-speed1").text("Wind Speed : " + response.list[0].wind.speed);
-			var humidity = $(".humidity1").text("Humidity : " + response.list[0].main.humidity +" %");
-			var date = $(".dateOf1").text("Current Date : " + response.list[0].dt_txt);	
+				for (i = 0; i < 40; i += 8) {
+					var temp = response.list[i].main.temp;
+					temp = Math.floor(temp * 9 / 5 - 459);
+					console.log(temp)
+					temp = temp + "°F"
+
+					var tempDescription = $(".temp-description1").text("Daily Forecast : " + response.list[i].weather[0].description)
+					// var tempImage = $("<img>").attr("src", response.list[0].weather[0].icon);
+					var cityOf = $("<p1>").text("Weather Details For : " + response.city.name);
+					var windSpeed = $("<p>").text("Wind Speed : " + response.list[i].wind.speed + "mph");
+					var humidity = $("<p>").text("Humidity : " + response.list[i].main.humidity + " %");
+					var date = response.list[i].dt_txt
+					console.log("this is date pre format", date)
+					date = moment().format('D')
+					console.log("this is date post format", date)
+					date = $("<p>").text("Current Date : " + response.list[i].dt_txt);
+					
+					var div = $("<div class='container weather'>");
+					div.append(cityOf, date, temp, humidity, windSpeed, tempDescription);
+					$("#weatherCard1").append(div)
+				}
 		}
-			function displayFutureWeatherOne(response) {
-			//for loop to go through the list and tell it to print out the stats of that corresponding day into a new div
-			console.log(response.list[8])
-			var temp = response.list[8].main.temp;
-			temp = Math.floor(temp * 9 / 5 - 459);
-			var temperature = $(".temperature2").text(temp + " ° F");
-			var tempDescription = $(".temp-description2").text("Daily Forecast : " + response.list[8].weather[0].description)
-			// var tempImage = $("<img>").attr("src", response.list[0].weather[0].icon);
-			var cityOf = $(".city2").text("Weather Details For : " + response.city.name);
-			var windSpeed = $(".wind-speed2").text("Wind Speed : " + response.list[8].wind.speed);
-			var humidity = $(".humidity2").text("Humidity : " + response.list[8].main.humidity +" %");
-			var date = $(".dateOf2").text("Current Date : " + response.list[8].dt_txt);
-		}
-		function displayFutureWeatherTwo(response) {
-			//for loop to go through the list and tell it to print out the stats of that corresponding day into a new div
-			console.log(response.list[16])
-			var temp = response.list[16].main.temp;
-			temp = Math.floor(temp * 9 / 5 - 459);
-			var temperature = $(".temperature3").text(temp + " ° F");
-			var tempDescription = $(".temp-description3").text("Daily Forecast : " + response.list[16].weather[0].description)
-			// var tempImage = $("<img>").attr("src", response.list[0].weather[0].icon);
-			var cityOf = $(".city3").text("Weather Details For : " + response.city.name);
-			var windSpeed = $(".wind-speed3").text("Wind Speed : " + response.list[16].wind.speed);
-			var humidity = $(".humidity3").text("Humidity : " + response.list[16].main.humidity +" %");
-			var date = $(".dateOf3").text("Current Date : " + response.list[16].dt_txt);
-		}
-		function displayFutureWeatherThree(response) {
-			//for loop to go through the list and tell it to print out the stats of that corresponding day into a new div
-			console.log(response.list[24])
-			var temp = response.list[24].main.temp;
-			temp = Math.floor(temp * 9 / 5 - 459);
-			var temperature = $(".temperature4").text(temp + " ° F");
-			var tempDescription = $(".temp-description4").text("Daily Forecast : " + response.list[24].weather[0].description)
-			// var tempImage = $("<img>").attr("src", response.list[0].weather[0].icon);
-			var cityOf = $(".city4").text("Weather Details For : " + response.city.name);
-			var windSpeed = $(".wind-speed4").text("Wind Speed : " + response.list[24].wind.speed);
-			var humidity = $(".humidity4").text("Humidity : " + response.list[24].main.humidity + " %");
-			var date = $(".dateOf4").text("Current Date : " + response.list[24].dt_txt);
-		}
-		function displayFutureWeatherFour(response) {
-			//for loop to go through the list and tell it to print out the stats of that corresponding day into a new div
-			console.log(response.list[32])
-			var temp = response.list[32].main.temp;
-			temp = Math.floor(temp * 9 / 5 - 459);
-			var temperature = $(".temperature5").text(temp + " ° F");
-			var tempDescription = $(".temp-description5").text("Daily Forecast : " + response.list[32].weather[0].description)
-			// var tempImage = $("<img>").attr("src", response.list[0].weather[0].icon);
-			var cityOf = $(".city5").text("Weather Details For : " + response.city.name);
-			var windSpeed = $(".wind-speed5").text("Wind Speed : " + response.list[32].wind.speed);
-			var humidity = $(".humidity5").text("Humidity : " + response.list[32].main.humidity +" %");
-			var date = $(".dateOf5").text("Current Date : " + response.list[32].dt_txt);
-		}
+		
 	})
 })
 //response.list[0].main.temp * 9 / 5 - 459 ** for loop i = list.length ** run through i + (however many 3-4 to next day)
