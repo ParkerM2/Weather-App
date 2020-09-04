45b5c6247fda9409dab692b54daac3a4f0418dbd
Parker Manning <parkerlmanning@hotmail.com>
2020-09-04 03:29:33 -0500

    finished setting up storage of api weather response for each separate day, and fixed onclick hide/show function for site

--- assets/weather.js
+++ assets/weather.js
@@ -5,10 +5,8 @@ $(document).ready(function () {
 
 
     // storage for the weather api response
-    //listDay is the original 5 days data :: 0-4 are each separate day's data
-    var responseObj = {
-        listDay: []
-    };
+    //listDay is the original 5 days data :: 1-5 are each separate day's data
+    var listDay = [];
     var day1 = [];
     var day2 = [];
     var day3 = [];
@@ -27,55 +25,37 @@ $(document).ready(function () {
         $.ajax(response).done(function (response) {
             let city = response.city.name;
             $("#city").html(city);
-            // for loop for grabbing the next day in the array of timestamps 
-            for (i = 1; i < 40; i = i + 8) {
-                // setting variable for each day of the 5 day forecast
+        // for loop for grabbing the next day in the array of timestamps 
+            for (i = 0; i < 40; i = i + 8) {
+            // setting variable for each day of the 5 day forecast
                 let list = response.list[i];
-                responseObj.listDay.push(list);
-                // setup local.storage here in future?
+                listDay.push(list);
+            // setup local.storage here in future?
             }
-            console.log(responseObj.listDay)
-        // Input = array of all days
-        // Output = object with separate arrays of weather data for each day
-            for (i = 1; i < 7; i++) {
-                let list = responseObj.listDay[i]
-                obj.days.push(list)
-                // console.log("second List", list)
-            }
-            console.log(obj)
+        // parsing all of the data for each day to a corresponding array
+            dayParse();
         })
-    };
-
- // This will generate the variables needed   
-    function createVariables() {
-        // Cycles through the array of days and sets variables for the response info
-            
-            var date = day.dt_txt;
-            var temp = day.main.temp;
-            var feelsLike = day.main.temp;
-            var humidity = day.main.humidity;
-            var weatherD = day.weather.description;
-            var weatherIcon = day.weather.weatherIcon;
-            var iconID = day.weather.id;
-            var wind = day.wind.speed + "mph";
-        console.log(date,temp,feelsLike,humidity,weatherD,weatherIcon,iconId,wind)
-    }
-    
-    
+    // function to grab each day's data and store it in its own array for easy grabbing
+        function dayParse() {
+            day1.push(listDay[0])
+            day2.push(listDay[1])
+            day3.push(listDay[2])
+            day4.push(listDay[3])
+            day5.push(listDay[4])
+        };
+    }; 
      // City Text
     var citySpan = $('<h2 id="city" class="w3-text w3-jumbo">');
     $("#spanContainer").html(citySpan);
 
-    // function showWeather() {
-	// 	$("#mainWeather").addClass("w3-show");
-	// 	$("#mainWeather").removeClass("w3-hide")
-	// 	console.log($("#mainWeather").prop("classList"))
-	
+    function showWeather() {
+        $("#mainWeather").removeClass("w3-hide")
+    }
     $("#searchBtn").on("click", function () {
             event.preventDefault();
             displayForecastWeather();
-            createVariables();
-            // showWeather();
+            // createVariables();
+            showWeather();
             // storeCityArray();
             // bringCityArray();
     });
